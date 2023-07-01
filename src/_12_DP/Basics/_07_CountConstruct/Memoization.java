4bd4884e6171381449dc7c97962dec05c8f440e6
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-01 22:28:36 +0530

    Update

--- src/_12_DP/Basics/_07_CountConstruct/Memoization.java
+++ src/_12_DP/Basics/_07_CountConstruct/Memoization.java
@@ -1,12 +1,28 @@
-//package _12_DP.Basics._07_CountConstruct;
-//
-//import java.util.HashMap;
-//
-//public class Memoization {
-//    public static void main(String[] args) {
-//        System.out.println(countConstruct("abcdef", new String[]{"ab", "abc", "cd", "def", "abcd"}, new HashMap<>())); // true
-//        System.out.println(countConstruct("skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}, new HashMap<>())); // false
-//        System.out.println(countConstruct("enterapotentpot", new String[]{"a", "p", "ent", "enter", "ot", "o", "t"}, new HashMap<>())); // true
-//        System.out.println(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[]{"e", "eee", "eeee", "eeeeeee", "eeeeee", "eeeeeeeeeeeeee", "ee"}, new HashMap<>())); // true
-//    }
-//}
+package _12_DP.Basics._07_CountConstruct;
+
+import java.util.HashMap;
+
+public class Memoization {
+    public static void main(String[] args) {
+        System.out.println(countConstruct("abcdef", new String[]{"ab", "abc", "cd", "def", "abcd"}, new HashMap<>())); // true
+        System.out.println(countConstruct("skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}, new HashMap<>())); // false
+        System.out.println(countConstruct("enterapotentpot", new String[]{"a", "p", "ent", "enter", "ot", "o", "t"}, new HashMap<>())); // true
+        System.out.println(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[]{"e", "eee", "eeee", "eeeeeee", "eeeeee", "eeeeeeeeeeeeee", "ee"}, new HashMap<>())); // true
+    }
+
+    static int countConstruct(String target, String[] arr, HashMap<String, Integer> memo) {
+        if (target.equals("")) return 1;
+        if (memo.containsKey(target)) return memo.get(target);
+
+        int count = 0;
+        for (String pre : arr) {
+            if (target.indexOf(pre) == 0) {
+                String suff = target.substring(pre.length());
+                int numOfWays = countConstruct(suff, arr, memo);
+                count += numOfWays;
+            }
+        }
+        memo.put(target, count);
+        return count;
+    }
+}
---
+++ src/_12_DP/Basics/_08_AllConstruct/BruteForce.java
@@ -0,0 +1,33 @@
+package _12_DP.Basics._08_AllConstruct;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        System.out.println(allConstruct("purple", new String[] {"purp", "p", "ur", "le", "purpl"}));
+        // Output: [[purp, le], [p, ur, p, le]]
+        System.out.println(allConstruct("abcdef", new String[] {"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
+        // Output: [[ab, cd, ef], [ab, c, def], [abc, def]]
+    }
+
+    static List<List<String>> allConstruct(String target, String[] arr) {
+        List<List<String>> result = new ArrayList<>();
+        if (target.equals("")) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+
+        for (String pre : arr) {
+            if (target.indexOf(pre) == 0) {
+                String suff = target.substring(pre.length());
+                List<List<String>> allPaths = allConstruct(suff, arr);
+                for (List<String> path : allPaths) {
+                    path.add(0, pre);
+                    result.add(path);
+                }
+            }
+        }
+        return result;
+    }
+}
