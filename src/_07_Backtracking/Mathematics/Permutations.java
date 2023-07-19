52deae8c6079765ac72ce26b5f41704e6031f0fe
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 09:42:04 +0530

    Update

--- src/_07_Backtracking/Mathematics/Permutations.java
+++ src/_07_Backtracking/Mathematics/Permutations.java
@@ -1,33 +1,45 @@
 package _07_Backtracking.Mathematics;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Permutations {
     public static void main(String[] args) {
-        System.out.println("Permutations : " + permutationsRecursive(new int[]{1,2,3}));
+        // STRING
+        List<String> ans = new ArrayList<>();
+        System.out.println("Count = " + permutationsString("", "abc", ans) + "\n" + ans + "\n");
+        // ARRAY / LIST
+        List<List<Integer>> answer = new ArrayList<>();
+        System.out.println("Count = " + permutationsArray(new ArrayList<>(), new int[]{1,2,3}, 0, answer) + "\n" + answer);
     }
 
-    public static List<List<Integer>> permutationsRecursive(int[] nums) {
-        return helper(0, nums);
+    static int permutationsString(String p, String up, List<String> ans) {
+        int count = 0;  //**
+        if (up.isEmpty()) {
+            ans.add(p);
+            return 1;   //any call / base calls
+        }
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String l = p.substring(i);
+            count += permutationsString(f + ch + l, up.substring(1), ans);
+        }
+        return count;   //1st call**
     }
 
-    public static List<List<Integer>> helper(int i, int[] nums) {
-        if (i == nums.length) {
-            List<List<Integer>> res = new ArrayList<>();
-            res.add(new ArrayList<>());
-            return res;
+    static int permutationsArray(List<Integer> p, int[] up, int ind, List<List<Integer>> ans) {
+        int count = 0;
+        if (ind == up.length) { //up is empty logically
+            ans.add(new ArrayList<>(p));
+            return 1;
         }
-        List<List<Integer>> resPerms = new ArrayList<>();
-        List<List<Integer>> perms = helper(i + 1, nums);
-        for (List<Integer> p : perms) {
-            for (int j = 0; j < p.size() + 1; j++) {
-                List<Integer> pCopy = new ArrayList<>();
-                pCopy.addAll(p);
-                pCopy.add(j, nums[i]);
-                resPerms.add(pCopy);
-            }
+        int ch = up[ind]; //first index of up string
+        for (int i = 0; i <= p.size(); i++) {
+            List<Integer> newp = new ArrayList<>(p);
+            newp.add(i, ch);
+            count += permutationsArray(newp, up, ind + 1, ans);
         }
-        return resPerms;
+        return count;
     }
 }
