d2284324b2e358af86e7fc8395f0f7238d501b24
Devansh Singh <devanshsingh0228@gmail.com>
2023-08-05 11:53:57 +0530

    Update

---
+++ src/_06_Trees/BinarySearchTree.java
@@ -0,0 +1,146 @@
+package _06_Trees;
+
+import java.util.Scanner;
+
+public class BinarySearchTree {
+    static class Node {
+        int value;
+        Node left, right;
+        int height;
+        Node(int value) {
+            this.value = value;
+            // rest all default values (height = 0)
+        }
+    }
+    Node root; // BST has a node root
+
+
+    public void input(Scanner sc) {
+        System.out.println("Start filling the Binary Search Tree :-" );
+        String inp = sc.next();
+        while (!inp.equals("e") && !inp.equals("ex") && !inp.equals("exit")) {
+            populate(Integer.parseInt(inp));
+            inp = sc.next();
+        }
+    }
+    public void populate(int key) {
+        root = populate(root, key);
+    }
+    private Node populate(Node node, int key) {
+        if (node == null) return new Node(key);
+        // insertion
+        if (key < node.value) node.left = populate(node.left, key);
+        else node.right = populate(node.right, key);
+
+        node.height = Math.max(height(node.right), height(node.left)) + 1; //inserted one node
+
+        return node; // return current/child node to its parent node
+    }
+    private void populateSorted(int[] nums, int start, int end) {
+        if (start >= end) return;
+
+        int mid = start + (end - start)/2;  // (start + end)/2 -> may give error in large sizes
+        populate(nums[mid]);
+        populateSorted(nums, start, mid);
+        populateSorted(nums, mid + 1, end);
+    }
+
+
+    public void displayTree() {
+        displayTree(root, 0);
+    }
+    private void displayTree(Node node, int level) {
+        if (node == null) return;
+        displayTree(node.right, level + 1);
+        // now "node" at right most level
+        if (level == 0) System.out.println(node.value);
+        else {
+            for (int i = 0; i < level - 1 ; i++) System.out.print("|\t\t");
+            System.out.println("|------>" + node.value);
+        }
+        displayTree(node.left, level + 1);
+    }
+
+
+    public void inorderSeq() {
+        System.out.print("Inorder/Sorted Sequence : [");
+        inorder(root);
+        System.out.println("\b\b]");  //removed 1 space & 1 coma at the end
+    }
+    private void inorder(Node node) {
+        if (node == null) return;
+
+        inorder(node.left);
+        System.out.print(node.value + ", ");
+        inorder(node.right);
+    }
+    public void preorderSeq() {
+        System.out.print("Preorder Sequence : [");
+        preorder(root);
+        System.out.println("\b\b]");
+    }
+    private void preorder(Node node) {
+        if (node == null) return;
+
+        System.out.print(node.value + ", ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+    public void postorderSeq() {
+        System.out.print("Postorder Sequence : [");
+        postorder(root);
+        System.out.println("\b\b]");
+    }
+    private void postorder(Node node) {
+        if (node == null) return;
+
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value + ", ");
+    }
+
+
+    public void balanced() {
+        String det;
+        if (balanced(root)) det = "";
+        else det = "NOT ";
+        System.out.println("Since the ΔHEIGHT b/w CHILDREN of EACH NODE throughout the tree IS "
+                + det + "<= 1 :-\n" + det + "BALANCED");
+    }
+    private boolean balanced(Node node) {
+        if (node == null) return true; //basically fallback
+
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+
+    private int height(Node node) {
+        if (node == null) return -1;
+        return node.height;
+    }
+    private boolean isEmpty() {
+        return root == null;
+    }
+
+
+    private void sampleTree() {
+//        int[] tree = {15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9}; //un-balanced
+        int[] tree = {15, 6, 18, 3, 7, 17, 20, 2, 4, 13}; //balanced
+//        int[] tree = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //sorted/skewed
+
+        for (int key : tree) root = populate(root, key);
+//        populateSorted(tree, 0, tree.length - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree bst = new BinarySearchTree();
+//        bst.input(sc);
+        bst.sampleTree();  System.out.println();
+        bst.displayTree(); System.out.println();
+        bst.inorderSeq();
+        bst.preorderSeq();
+        bst.postorderSeq();System.out.println();
+        bst.balanced();    System.out.println();
+    }
+}
---
+++ src/_06_Trees/BinaryTree.java
@@ -0,0 +1,77 @@
+package _06_Trees;
+
+import java.util.Scanner;
+
+public class BinaryTree {
+   static class Node {
+        int value;
+        Node left, right;
+        Node(int value) {
+            this.value = value;
+        }
+   }
+
+    Node root;  //global-variable / instance-v. / static-v.? => Call by Reference
+
+    //insert elements [DFS]
+    public void populate(Scanner sc) {  //user doesn't need to know & write "root" for him/herself
+        System.out.println("Enter the root node value : ");
+        int value = sc.nextInt();
+        root = new Node(value);
+        populate(sc, root);
+    }
+    private void populate(Scanner sc, Node node) {
+        System.out.println("Do you want to enter LEFT of " + node.value + " ?");
+        char choice = sc.next().charAt(0);
+        if (choice == 'y') {
+            System.out.print("=> ");
+            node.left = new Node(sc.nextInt());
+            populate(sc, node.left);
+        }
+        System.out.println("Do you want to enter RIGHT of " + node.value + " ?");
+        choice = sc.next().charAt(0);
+        if (choice == 'y') {
+            System.out.print("=> ");
+            node.right = new Node(sc.nextInt());
+            populate(sc, node.right);
+        }
+    }
+    //pretty display
+    public void display() { //user doesn't need to know & write "root" for him/herself
+        display(root, 0);
+    }
+    private void display(Node node, int level) {
+        if (node == null) return;
+        display(node.right, level + 1); //gives right most node
+
+        if (level == 0) {
+            System.out.println(node.value);
+        } else {
+            for (int i = 0; i < level - 1; i++) System.out.print("|\t\t");
+            System.out.println("|------>" + node.value);
+        }
+        display(node.left, level + 1);
+    }
+
+    private void sampleTree() {
+        root = new Node(15);
+        Node node1 = new Node(6);
+        Node node2 = new Node(9);
+        Node node3 = new Node(8);
+        Node node4 = new Node(14);
+        Node node5 = new Node(10);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node3.right = node5;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinaryTree bt = new BinaryTree();
+//        BT.populate(sc);
+        bt.sampleTree();
+        bt.display();
+    }
+}
